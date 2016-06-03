179188a33fdc7a861eaefb2fe3fae50a3ee2c516
Sam Tresler <sam@treslervania.com>
2016-06-03 16:48:16 -0400

    Dovecot Docker build working - needs testing.

--- dovecot/entrypoint.sh
+++ dovecot/entrypoint.sh
@@ -6,6 +6,10 @@ confd -onetime -backend env
 chown root:root /etc/dovecot/dovecot-sql.conf.ext
 chmod go= /etc/dovecot/dovecot-sql.conf.ext
 
+echo ${ISPMAIL_SSL_CERT} > /etc/dovecot/ssl/dovecot.pem
+echo ${ISPMAIL_SSL_KEY} > /etc/dovecot/ssl/dovecot.key
+
+
 # Start Dovecot 
 exec /bin/true
 
